3ebefcbe38bf437c52e3494fc960c547f854a263
ankrypt <ankitc@us.ibm.com>
2016-12-04 15:06:10 -0600

    added deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 set -o errexit -o nounset
 
+git config --global user.email "ankitc@gmail.com"
+git config --global user.name "ankrypt"
+
 git add -A .
 git commit -m "Uploading link metadata"
 git push
