69def534c079aed69e1b4e8d6c7a9392e4d21c30
ankrypt <ankitc@us.ibm.com>
2016-12-04 15:59:17 -0600

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 ##set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
-git remote add origin ankrypt/in-toto.git
+
 git config --global user.email "ankitc@gmail.com"
 git config --global user.name "ankrypt"
 git config credential.helper "store --file=.git/credentials"
@@ -13,4 +13,4 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git add -A .
 git status
 git commit -m "Uploading link metadata"
-git push origin master
+git push origin
