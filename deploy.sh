b5e50a700f3907e526e3014d40525ba3f55c82fe
ankrypt <ankitc@us.ibm.com>
2016-12-04 16:19:59 -0600

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 ##set -o errexit -o nounset
 
 #rev=$(git rev-parse --short HEAD)
-git remote set-url testorigin git@github.com:ankrypt/in-toto.git
+git remote set-url origin git@github.com:ankrypt/in-toto.git
 git config --global user.email "ankitc@gmail.com"
 git config --global user.name "ankrypt"
 git config credential.helper "store --file=.git/credentials"
@@ -13,7 +13,7 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git add -A .
 git status
 git commit -m "Uploading link metadata"
-git push testorigin
+git push origin
 
 #git tag -l 56 # timeout=10
 #git tag -a -f -m Jenkins Git plugin tagging with 56 56 # timeout=10
