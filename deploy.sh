3d77bd80083f0a3b294d0d33ee34a3fec9db2787
ankrypt <ankitc@us.ibm.com>
2016-12-04 18:07:39 -0600

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 set -ev
-git add -A .
-git config --global user.email "ankitc@gmail.com"
-git config --global user.name "ankrypt"
-git config --global push.default simple
-git commit -am "upload link metadata"
-git push https://$GH_TOKEN@github.com/ankrypt/in-toto HEAD:develop
+
+new_chk=$(find ./in-toto/ -type f -exec md5sum {} \; | sort -k 2 | md5sum  | tr -d '\r ')
+orig_chk=$(cat ./project.chk | tr -d '\r ') 
+
+echo $new_chk
+echo $orig_chk
+
+if [[ "$new_chk" == "$orig_chk" ]] ; then
+    # if same checksums, then abort this push
+    echo "[-]nothing changed, abort this push..." 1>&2
+    exit 1
+else
+    echo -ne $(find ./in-toto/ -type f -exec md5sum {} \; | sort -k 2 | md5sum) > project.chk
+    echo "[+]renew chksum file..."
+    git add -A .
+    git config --global user.email "ankitc@gmail.com"
+    git config --global user.name "ankrypt"
+    git config --global push.default simple
+    git commit -am "upload link metadata"
+    git push https://$GH_TOKEN@github.com/ankrypt/in-toto HEAD:develop
+fi
